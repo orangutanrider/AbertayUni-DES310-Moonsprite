b866b88d8187f6d920df85ad53521563a235d83e
Dominic Rooney <orangutanrider@gmail.com>
2023-03-16 13:57:27 +0000

    Update PlayerInventoryController.cs

--- Moonsprite-Unity-Project/Assets/Our Files/Scripts/PlayerInventoryController.cs
+++ Moonsprite-Unity-Project/Assets/Our Files/Scripts/PlayerInventoryController.cs
@@ -7,12 +7,6 @@ public class PlayerInventoryController : MonoBehaviour
     [Header("Required References")]
     public GameObject toolBarUIObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
