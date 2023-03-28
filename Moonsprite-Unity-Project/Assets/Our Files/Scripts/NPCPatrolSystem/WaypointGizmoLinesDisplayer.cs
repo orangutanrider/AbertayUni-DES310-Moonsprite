a5257933d676f2e702231c6711d6838689b3e9f9
DOMINIC ROONEY <orangutanrider@gmail.com>
2023-03-28 14:05:15 +0100

    Gizmos line displayer
    it makes it easier to see the waypoint path

---
+++ Moonsprite-Unity-Project/Assets/Our Files/Scripts/NPCPatrolSystem/WaypointGizmoLinesDisplayer.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointGizmoLinesDisplayer : MonoBehaviour
+{
+    public bool displayingGizmos = false;
+    [Space]
+    public float radiusDisplay = 0.25f;
+    [Space]
+    public bool setWaypointsToChildObjectsBUTTON = false;
+    public List<Transform> waypoints = new List<Transform>();
+
+    void OnDrawGizmos()
+    {
+        if(displayingGizmos == false)
+        {
+            return;
+        }
+
+        DrawCircleGizmosOnWaypoints();
+        DrawLineGizmosBetweenWaypoints();
+    }
+
+    void DrawCircleGizmosOnWaypoints()
+    {
+        foreach(Transform waypoint in waypoints)
+        {
+            Gizmos.DrawWireSphere(waypoint.position, radiusDisplay);
+        }
+    }
+
+    void DrawLineGizmosBetweenWaypoints()
+    {
+        for (int loop = 0; loop < waypoints.Count; loop++)
+        {
+            if(loop + 1 < waypoints.Count)
+            {
+                Gizmos.DrawLine(waypoints[loop].position, waypoints[loop + 1].position);
+            }
+            else
+            {
+                Gizmos.DrawLine(waypoints[loop].position, waypoints[0].position);
+            }
+        }
+    }
+
+    void OnValidate()
+    {
+        SetWaypointsToChildObjects();
+    }
+
+    void SetWaypointsToChildObjects()
+    {
+        if (setWaypointsToChildObjectsBUTTON == false)
+        {
+            return;
+        }
+        setWaypointsToChildObjectsBUTTON = true;
+
+        waypoints.Clear();
+
+        Transform[] childObjects = gameObject.GetComponentsInChildren<Transform>();
+        foreach (Transform child in childObjects)
+        {
+            if (child == gameObject.transform)
+            {
+                continue;
+            }
+
+            waypoints.Add(child);
+        }
+    }
+}
