fb53aaf219af6a543a684289a98c62c4a3e67971
Mad-Jacks-Wonderland <54810548+Mad-Jacks-Wonderland@users.noreply.github.com>
2023-04-19 15:35:35 +0100

    dialogue sound playing
    yes

---
+++ Moonsprite-Unity-Project/Assets/CoroutineTest.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class CoroutineTest : MonoBehaviour
+{
+    void Start()
+    {
+        //Start the coroutine we define below named ExampleCoroutine.
+        StartCoroutine(ExampleCoroutine( 2, 5));
+    }
+
+    IEnumerator ExampleCoroutine(int x, int y)
+    {
+
+        string[] test = Regex.Split("AEIOU", string.Empty);
+
+        foreach (string s in test)
+        {
+            Debug.LogWarning("current Sound: " + s);
+        }
+
+        string testingString = "this drill";
+        int letter = 3;
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        //After we have waited 5 seconds print the time again.
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+
+        Debug.Log("Numbers are : " + x + " and " + y);
+
+        Debug.Log("letter number " + letter + " is " + testingString[letter]);
+
+        //FindObjectOfType<AudioManager>().Play("123");
+        FindObjectOfType<AudioManager>().playDialogue();
+    }
+}
