8af8ea52671ebce1d8c1df8257f9dd9ea824bb4d
magalhaess10 <91020181+magalhaess10@users.noreply.github.com>
2023-05-03 12:21:26 +0100

    transitions

---
+++ Moonsprite-Unity-Project/Assets/Our Files/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           // transition.SetTrigger("TransitionStart");
+            //Invoke("LoadNextLevel", 1f);
+        }
+        
+    }
+
+    public void StartTransition()
+    {
+        transition.SetTrigger("TransitionStart");
+        Invoke("LoadNextLevel", 1f);
+        Debug.Log("pressed");
+    }
+    void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
